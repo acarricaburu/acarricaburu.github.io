da5f247defc1d61dacc1021d5e2b7f6b1704a211
acarricaburu <105254960+acarricaburu@users.noreply.github.com>
2022-05-12 12:40:24 -0300

    Create 1-meuPrimeiroPrograma.js

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu Primeiro Programa: trabalhando com variáveis");
+
+const idade = 21;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
